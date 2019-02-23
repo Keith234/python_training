7b874daeec7e5557515075bfe90199718f704995
Keith234 <lebedevamarina623@gmail.com>
2019-02-24 00:58:05 +0200

    Added test_add_contact

---
+++ contact.py
@@ -0,0 +1,27 @@
+class Contact:
+
+    def __init__(self, first_name, middlename, lastname, nicknam, title, company, address, home, mobile, work, fax,
+                 email, email2, email3, bday,bmonth, byear, aday, amonth,  ayear, address2, phone2, notes):
+        self.first_name = first_name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nicknam = nicknam
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self. ayear = ayear
+        self. address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
