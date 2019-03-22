ec1fdc0678ee6f9a390469a65c8eec85fb94ca80
Keith234 <lebedevamarina623@gmail.com>
2019-03-23 00:49:55 +0200

    Генерация тестовых данных в формате JSON (начало)

--- conftest.py
+++ conftest.py
@@ -6,7 +6,6 @@ from fixture.application import Application
 fixture = None
 target = None
 
-
 @pytest.fixture
 def app(request):
     global fixture
@@ -16,7 +15,7 @@ def app(request):
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
         with open(config_file) as f:
             target = json.load(f)
-    if fixture is None or fixture.is_valid():
+    if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target["baseUrl"])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
@@ -27,7 +26,6 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
 
